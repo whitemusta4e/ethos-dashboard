e0c5bf41c774d0f95387d9751a5f988940fef3bd
whitemusta4e <whitemusta4e@gmail.com>
2025-07-16 14:07:31 +0300

    Delete pages/api directory

--- pages/api/webhook.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-  if (req.method === 'POST') {
-    console.log('Отримано дані:', req.body);
-    res.json({ success: true });
-  } else {
-    res.json({ message: 'API працює' });
-  }
-}
